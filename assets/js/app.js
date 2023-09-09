f51a919bbd669db75121dccca2ccb4f423ca8f16
soheil <jalilisoheil14@gmail.com>
2023-09-08 18:28:38 -0700

    add header feature

---
+++ assets/js/app.js
@@ -0,0 +1,14 @@
+new fullpage("#fullpage", {
+  //options here
+  autoScrolling: true,
+  navigation: true,
+  navigationTooltips: [
+    "صفحه نخست",
+    "خدمات ما",
+    "محصولات ما",
+    "تماس با ما",
+    "درباره ما",
+  ],
+  showActiveTooltip: true,
+  //   scrollHorizontally: true,
+});
